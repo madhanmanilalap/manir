e83260d033cab50ba269deaf2deb9c6efdd044bb
madhanmanilalap <35652995+madhanmanilalap@users.noreply.github.com>
2018-01-25 15:43:33 +0530

    Create mm.c

---
+++ mm.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the value a");
+scanf("%d",&a);
+if(a%4==0)
+printf("Leap year");
+else
+printf("Not leap year");
+getch();
+}
